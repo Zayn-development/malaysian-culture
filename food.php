4f6b3de87e26c50abf3a177aa04f203cb5e97122
Zayn-development <dcsy23060010@unikop.edu.my>
2024-09-03 10:58:15 +0800

    Update food.php

--- food.php
+++ food.php
@@ -40,7 +40,7 @@ error_reporting(E_ALL);
         while ($row = $result->fetch_assoc()) {
             echo '<h3>' . $row['food_name'] . '</h3>';
             echo '<p>' . $row['description'] . '</p>';
-            echo '<img src="images/' . $row['image'] . '" alt="' . $row['food_name'] . '">';
+            echo '<img src="' . $row['image'] . '" alt="' . $row['food_name'] . '">';
         }
     } else {
         echo "No foods found.";
